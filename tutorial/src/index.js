e3abe0cf290d1004a2f6af7e6cf443e24505b493
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-20 18:54:39 +0200

    JSX-Javascript and how to access

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -8,45 +8,25 @@ function BookList() {
   return (
     <section className='booklist'>
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   )
 }
 
+const author = 'Amelia Hepworth'
 const Book = () => {
+  const title = 'I love you to the Moon and Back'
+
   return (
     <article className='book'>
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src='https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSPtxH9mRAD-2H6LxkUIH-sfzPmnMjgl6XSJ7mnWMl4lcJXSZEXjeD3SWY5Gla_3kycT52L3b1_w4DA0TEeo3DBocU7ekit8xlDXwK6F4Z7&usqp=CAE'
+        alt=''
+      />
+      <h1>{title}</h1>
+      <h4>{author.toUpperCase()}</h4>
+      {/* <p>{let x = 6}</p> */}
+      <p>{6 + 6}</p>
     </article>
-  )
-}
-
-const Image = () => {
-  return (
-    <img
-      src='https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSPtxH9mRAD-2H6LxkUIH-sfzPmnMjgl6XSJ7mnWMl4lcJXSZEXjeD3SWY5Gla_3kycT52L3b1_w4DA0TEeo3DBocU7ekit8xlDXwK6F4Z7&usqp=CAE'
-      alt=''
-    />
-  )
-}
-
-const Title = () => {
-  return <h1>I love you to the Moon and Back</h1>
-}
-
-const Author = () => {
-  return (
-    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
-      Amelia Hepworth
-    </h4>
   )
 }
 
