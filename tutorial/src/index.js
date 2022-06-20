8ce9347dbaa172aaee73f59e5c28d1ae593d827c
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-20 17:55:32 +0200

    JSX rules, forst component setup / functional stateless component

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,10 +1,44 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+// stateless functional component (dumb component)
+// always return JSX
+
+// JSX Rules
+// return single element
+// div / section / article or Fragment
+// use camelCase for html attribute
+// className instead of class
+// close every element
+// formatting
+
 function Greeting() {
-  return <h4>this is MARConthemove witch my first component</h4>
+  return (
+    <div>
+      <h3>hello people</h3>
+      <ul>
+        <li>
+          <a href='#'>hello world</a>
+        </li>
+        <img src='' alt='' />
+        <input type='text' name='' id='' />
+      </ul>
+    </div>
+  )
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+// function Greeting() {
+//   return <h4>hello world</h4>
+// }
+
+// const Greeting = () => {
+//   return React.createElement(
+//     'div',
+//     {},
+//     React.createElement('h1', {}, 'hello world')
+//   )
+// }
 
 export default Greeting
+
+ReactDom.render(<Greeting />, document.getElementById('root'))
