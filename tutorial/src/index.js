8d5ee273ca6a3e311fc24a0369cb6ff3f98c03bc
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-20 18:35:33 +0200

    JSX- CSS rule application -> higher priority than imported .css file

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -43,7 +43,11 @@ const Title = () => {
 }
 
 const Author = () => {
-  return <h4>Amelia Hepworth</h4>
+  return (
+    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.25rem' }}>
+      Amelia Hepworth
+    </h4>
+  )
 }
 
 export default BookList
