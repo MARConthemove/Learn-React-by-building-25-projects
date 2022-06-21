98867c6900e7c296ddf8e7d7f8a33e0b8d5cba03
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-21 14:19:51 +0200

    simple list rendering

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -5,42 +5,31 @@ import ReactDom from 'react-dom'
 import './index.css'
 
 // setup vars
-const firstBook = {
-  img: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSPtxH9mRAD-2H6LxkUIH-sfzPmnMjgl6XSJ7mnWMl4lcJXSZEXjeD3SWY5Gla_3kycT52L3b1_w4DA0TEeo3DBocU7ekit8xlDXwK6F4Z7&usqp=CAE',
-  title: 'I love you to the Moon and Back',
-  author: 'Amelia Hepworth',
-}
-const secondBook = {
-  img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRzqRfvu42P8Wm_ZJhTupc9VOH1foYuAbY_dmw26_zWoDH_Qrwo3X-abfFKH35OsvR5cX6grcI7ad0_qjOt7sSIhfeagAvFElVgqJNm8oR1wT0x0OiLoUDYgg&usqp=CAE',
-  title: 'Our Class is a Family',
-  author: 'Shannon Olsen',
-}
+const books = [
+  {
+    img: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSPtxH9mRAD-2H6LxkUIH-sfzPmnMjgl6XSJ7mnWMl4lcJXSZEXjeD3SWY5Gla_3kycT52L3b1_w4DA0TEeo3DBocU7ekit8xlDXwK6F4Z7&usqp=CAE',
+    title: 'I love you to the Moon and Back',
+    author: 'Amelia Hepworth',
+  },
+  {
+    img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRzqRfvu42P8Wm_ZJhTupc9VOH1foYuAbY_dmw26_zWoDH_Qrwo3X-abfFKH35OsvR5cX6grcI7ad0_qjOt7sSIhfeagAvFElVgqJNm8oR1wT0x0OiLoUDYgg&usqp=CAE',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen',
+  },
+]
+
+const names = ['john', 'peter', 'susan']
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+})
+console.log(newNames)
 
 function BookList() {
-  return (
-    <section className='booklist'>
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Necessitatibus quae commodi blanditiis laborum excepturi quisquam
-          atque modi nemo mollitia omnis, amet voluptatem quibusdam, eveniet qui
-          eos accusantium repudiandae, cumque dolorum?
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  )
+  return <section className='booklist'>{newNames}</section>
 }
 
 const Book = (props) => {
+  console.log(props)
   // Object destructuring!
   // Either destructur in the body of the function or in the parameter directly!
   const { img, title, author } = props
@@ -50,7 +39,6 @@ const Book = (props) => {
       <img src={img} alt='' />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {props.children}
     </article>
   )
 }
