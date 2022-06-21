5b7ee7ae32a949687a5ddd76d5bd3695aa4848e4
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-21 15:28:55 +0200

    proper list mapping

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -7,32 +7,40 @@ import './index.css'
 // setup vars
 const books = [
   {
+    id: 1,
     img: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcSPtxH9mRAD-2H6LxkUIH-sfzPmnMjgl6XSJ7mnWMl4lcJXSZEXjeD3SWY5Gla_3kycT52L3b1_w4DA0TEeo3DBocU7ekit8xlDXwK6F4Z7&usqp=CAE',
     title: 'I love you to the Moon and Back',
     author: 'Amelia Hepworth',
   },
   {
+    id: 2,
     img: 'https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcRzqRfvu42P8Wm_ZJhTupc9VOH1foYuAbY_dmw26_zWoDH_Qrwo3X-abfFKH35OsvR5cX6grcI7ad0_qjOt7sSIhfeagAvFElVgqJNm8oR1wT0x0OiLoUDYgg&usqp=CAE',
     title: 'Our Class is a Family',
     author: 'Shannon Olsen',
   },
+  {
+    id: 3,
+    img: 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTl0ua5w4QkRhlKNtjHwG7tFbKqeytIbxuXfZAixe64gqwsIInIZCbFAvHVV0iW-tamPukPmF--ra7OgSuGgt2lihBfZZeX13UyN3kXPwa_2Tp2XEY8mclK&usqp=CAE',
+    title: 'Feeling is the secret',
+    author: 'Neville Godd',
+  },
 ]
 
-const names = ['john', 'peter', 'susan']
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>
-})
-console.log(newNames)
-
 function BookList() {
-  return <section className='booklist'>{newNames}</section>
+  return (
+    <section className='booklist'>
+      {books.map((book) => {
+        return <Book key={book.id} book={book}></Book>
+      })}
+    </section>
+  )
 }
 
 const Book = (props) => {
   console.log(props)
   // Object destructuring!
   // Either destructur in the body of the function or in the parameter directly!
-  const { img, title, author } = props
+  const { img, title, author } = props.book
 
   return (
     <article className='book'>
