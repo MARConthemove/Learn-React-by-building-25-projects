b16278d93a87f3a2bcbe2735efbd8a905fa87c0c
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-22 19:04:07 +0200

    v1.0 / working but I want to edit the data handling and let the magic happen in the component

--- react-projects/01-birthday-reminder/setup/src/App.js
+++ react-projects/01-birthday-reminder/setup/src/App.js
@@ -3,16 +3,23 @@ import data from './data'
 import List from './List'
 
 function App() {
-  const [people, setPeople] = useState({ data })
+  const [people, setPeople] = useState(data)
 
-  console.log(people.data)
+  // console.log('people useState: ', people)
+
+  const clearListHandler = () => {
+    console.log('clear button pressed!')
+    setPeople([])
+  }
 
   return (
     <main>
       <section className='container'>
-        <h3>0 birthdays today</h3>
-        <List key={people.id} {...people} />
-        <button onClick={() => console.log('you clicked me')}>clear all</button>
+        <h3>{people.length} birthdays today</h3>
+        {people.map((person) => {
+          return <List key={person.id} {...person}></List>
+        })}
+        <button onClick={clearListHandler}>clear all</button>
       </section>
     </main>
   )
--- react-projects/01-birthday-reminder/setup/src/List.js
+++ react-projects/01-birthday-reminder/setup/src/List.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
 const List = (props) => {
-  console.log('propsList: ', props)
+  // console.log('props: ', props)
+  const { image, name, age } = props
 
   return (
     <React.Fragment>
       <article className='person'>
-        <img src={props.data[0].image} alt='test' />
+        <img src={image} alt='user_picture' />
         <div>
-          <h4>{props.data[0].name}</h4>
-          <p>26 years</p>
+          <h4>{name}</h4>
+          <p>{age} years</p>
         </div>
       </article>
-      <h2>test</h2>
     </React.Fragment>
   )
 }
