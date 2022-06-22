0eeae2db83de4cf78fc882541d22abec5350202f
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-22 18:31:10 +0200

    birthdayReminder App - implementing design

--- react-projects/01-birthday-reminder/setup/src/List.js
+++ react-projects/01-birthday-reminder/setup/src/List.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React from 'react'
 
-const List = () => {
+const List = (props) => {
+  console.log('propsList: ', props)
+
   return (
-    <>
-      <h2>list component</h2>
-    </>
-  );
-};
+    <React.Fragment>
+      <article className='person'>
+        <img src={props.data[0].image} alt='test' />
+        <div>
+          <h4>{props.data[0].name}</h4>
+          <p>26 years</p>
+        </div>
+      </article>
+      <h2>test</h2>
+    </React.Fragment>
+  )
+}
 
-export default List;
+export default List
