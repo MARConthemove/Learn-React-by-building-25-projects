27f97d8410267cdb7d0837a1a8463c162df50b77
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-23 14:31:28 +0200

    short-circuit evaluation

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello world'
+  const secondValue = text && 'hello world'
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <React.Fragment>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
 
-export default ShortCircuit;
+      {/* {if(){console.log("hello world")}} */}
+
+      {/* if text is true return text */}
+      <h1>{text || 'john doe'}</h1>
+
+      {/* if text is true then return <h1>hello world</h1> */}
+      {/* if text is false -> no return */}
+      {text && <h1>hello world</h1>}
+
+      {/* if text is not true -> return h1 */}
+      {!text && <h1>hello world</h1>}
+    </React.Fragment>
+  )
+}
+
+export default ShortCircuit
