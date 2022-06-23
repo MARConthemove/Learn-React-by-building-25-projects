ff61d74f106401dcd95fffa3ffc3b3f316949fde
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-23 15:34:55 +0200

    show and hide components

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,8 +4,9 @@ import React, { useState } from 'react'
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world'
-  const secondValue = text && 'hello world'
+  const [isError, setIsError] = useState(false)
+  // const firstValue = text || 'hello world'
+  // const secondValue = text && 'hello world'
 
   return (
     <React.Fragment>
@@ -16,13 +17,26 @@ const ShortCircuit = () => {
 
       {/* if text is true return text */}
       <h1>{text || 'john doe'}</h1>
+      <button className='btn' onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+
+      {isError && <h1>Error...</h1>}
 
       {/* if text is true then return <h1>hello world</h1> */}
       {/* if text is false -> no return */}
       {text && <h1>hello world</h1>}
 
       {/* if text is not true -> return h1 */}
-      {!text && <h1>hello world</h1>}
+      {/* {!text && <h1>hello world</h1>} */}
+
+      {isError ? (
+        <p>there is an error occured...</p>
+      ) : (
+        <div>
+          <h2>there is no error</h2>
+        </div>
+      )}
     </React.Fragment>
   )
 }
--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
-};
+  // state
+  const [show, setShow] = useState(false)
+  console.log(show)
+  return (
+    <React.Fragment>
+      <button className='btn' onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </React.Fragment>
+  )
+}
 
-export default ShowHide;
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth)
+
+  const checkSize = () => {
+    setSize(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize)
+    return () => {
+      window.removeEventListener('resize', checkSize)
+    }
+  }, [])
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', () => {
+  //     setSize(window.innerWidth)
+  //   })
+  // }, [])
+
+  return (
+    <div style={{ marginTop: '2rem' }}>
+      <h1>window</h1>
+      <h2>size : {size} px</h2>
+    </div>
+  )
+}
+
+export default ShowHide
