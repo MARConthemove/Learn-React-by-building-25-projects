66b883e7b5cfa79e5d88f2c3913112a15842efbf
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-23 13:19:53 +0200

    Mutliple returns - fetching evaluation

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -3,13 +3,68 @@ import React, { useState, useEffect } from 'react'
 const url = 'https://api.github.com/users/QuincyLarson'
 
 const MultipleReturns = () => {
-  const [loading, setLoading] = useState(true)
+  const [isloading, setIsLoading] = useState(true)
+  const [isError, setIsError] = useState(false)
+  const [user, setUser] = useState('default user')
 
-  if (loading) {
-    return <h2>Loading...</h2>
+  /*
+  const fetchData = async () => {
+    const response = await fetch(url)
+    console.log('response: ', response)
+    // console.log('typeof response: ', typeof response)
+
+    const user = await response.json()
+    console.log('user: ', user)
+    // console.log('typeof user: ', typeof user)
+
+    setUser(user)
+  }
+  */
+
+  useEffect(() => {
+    // setIsLoading(true) also possible here.
+    fetch(url)
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json()
+        } else {
+          setIsLoading(false)
+          setIsError(true)
+          // error throwing:
+          throw new Error(res.statusText)
+        }
+      })
+      .then((user) => {
+        const { login } = user
+        setUser(login)
+
+        setTimeout(() => {
+          setIsLoading(false)
+        }, 500)
+      })
+      .catch((err) => console.log(err)) // with this catch we are only catching network errors! (-> axios for handling)
+  }, [])
+
+  if (isloading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    )
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error!</h1>
+      </div>
+    )
   }
 
-  return <h2>multiple returns</h2>
+  return (
+    <div>
+      <h1>{user}</h1>
+    </div>
+  )
 }
 
 export default MultipleReturns
