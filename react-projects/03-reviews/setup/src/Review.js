69e0ce3781113bf495d79436f80c239d5120dfc8
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-24 12:53:56 +0200

    Review Project start

--- react-projects/03-reviews/setup/src/Review.js
+++ react-projects/03-reviews/setup/src/Review.js
@@ -1,9 +1,50 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  // state
+  const [index, setIndex] = useState(0)
 
-export default Review;
+  console.log(people)
+
+  // data destructuring
+  const { id, image, job, name, text } = people[index]
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img className='person-img' src={image} alt={name} />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button
+          className='prev-btn'
+          onClick={() => {
+            if (index === 0) return
+            setIndex(index - 1)
+          }}
+        >
+          <FaChevronLeft />
+        </button>
+        <button
+          className='next-btn'
+          onClick={() => {
+            if (index === index.length) return
+            setIndex(index + 1)
+          }}
+        >
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>random</button>
+    </article>
+  )
+}
+
+export default Review
