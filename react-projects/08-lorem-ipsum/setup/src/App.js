deca8f468b56ad08abe7a19569104d9e37e6ec2d
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-28 09:21:44 +0200

    Building a lorem ipsum paragraph builder

--- react-projects/08-lorem-ipsum/setup/src/App.js
+++ react-projects/08-lorem-ipsum/setup/src/App.js
@@ -1,11 +1,43 @@
-import React, { useState } from 'react';
-import data from './data';
+import React, { useState } from 'react'
+import data from './data'
+
 function App() {
-  
-  
+  const [paragraphs, setParagraphs] = useState(data)
+  const [amount, setAmount] = useState('')
+  const [displayed, setDisplayed] = useState([])
+
+  console.log(paragraphs)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if (amount) {
+    }
+  }
+
   return (
-  <h2>lorem ipsum project setup</h2>
-    )
+    <section className='section-center'>
+      <h3>tired of boring lorem ipsum?</h3>
+      <form action='' className='lorem-form' onSubmit={handleSubmit}>
+        <label htmlFor='amount'>paragraphs:</label>
+        <input
+          type='number'
+          id='amount'
+          name='amount'
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button className='btn'>generate</button>
+      </form>
+      {paragraphs.map((paragraph) => {
+        return (
+          <article className='lorem-text'>
+            <p>{paragraph}</p>
+          </article>
+        )
+      })}
+    </section>
+  )
 }
 
-export default App;
+export default App
