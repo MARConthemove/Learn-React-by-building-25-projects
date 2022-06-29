718699f010840926586b729c8d5c3b27a12249e6
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-29 10:38:01 +0200

    created a complex reducer function to handle dispatch actions in our app

--- react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
+++ react-advanced-2020/src/tutorial/6-useReducer/setup/index.js
@@ -3,27 +3,75 @@ import Modal from './Modal'
 import { data } from '../../../data'
 // reducer function
 
-const Index = () => {
-  const [people, setPeople] = useState(data)
-  const [showModal, setShowModal] = useState(false)
-  const [name, setName] = useState('')
+const reducer = (state, action) => {
+  console.log('state: ', state)
 
-  console.log(people)
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    if (name) {
-      setShowModal(true)
-      setPeople([...people, { id: new Date().getTime().toString(), name }])
-      setName('')
-    } else {
-      setShowModal(true)
+  if (action.type === 'ADD_ITEM') {
+    const newPeople = [...state.people, action.payload]
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item added',
     }
   }
 
+  if (action.type === 'NO_VALUE') {
+    return { ...state, isModalOpen: true, modalContent: 'please enter value' }
+  }
+
+  if (action.type === 'CLOSE_MODAL') {
+    return { ...state, isModalOpen: false }
+  }
+
+  if (action.type === 'REMOVE_ITEM') {
+    const newPeople = state.people.filter((person) => {
+      return person.id !== action.payload
+    })
+    return {
+      ...state,
+      people: newPeople,
+      isModalOpen: true,
+      modalContent: 'item removed',
+    }
+  }
+
+  throw new Error('no matching action type')
+}
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  modalContent: '',
+}
+
+const Index = () => {
+  // state:
+  const [name, setName] = useState('')
+  const [state, dispatch] = useReducer(reducer, defaultState)
+
+  // submit handler:
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      const newItem = { id: new Date().getTime().toString(), name }
+      dispatch({ type: 'ADD_ITEM', payload: newItem })
+      setName('')
+    } else {
+      dispatch({ type: 'NO_VALUE' })
+    }
+  }
+
+  const closeModal = () => {
+    dispatch({ type: 'CLOSE_MODAL' })
+  }
+
+  // render:
   return (
     <React.Fragment>
-      {showModal && <Modal />}
+      {state.isModalOpen && (
+        <Modal closeModal={closeModal} modalContent={state.modalContent} />
+      )}
       <form onSubmit={handleSubmit} className='form'>
         <div>
           <input
@@ -34,10 +82,17 @@ const Index = () => {
         </div>
         <button type='submit'>add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         return (
-          <div key={person.id}>
+          <div key={person.id} className='item'>
             <h4>{person.name}</h4>
+            <button
+              onClick={() =>
+                dispatch({ type: 'REMOVE_ITEM', payload: person.id })
+              }
+            >
+              remove
+            </button>
           </div>
         )
       })}
