5ff0e8e2ffe99232d761f1e417811b7a86bbcce9
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-30 09:22:44 +0200

    Stripe menu project started - for portfolio purpose

--- react-projects/13-stripe-submenus/setup/src/context.js
+++ react-projects/13-stripe-submenus/setup/src/context.js
@@ -1,2 +1,16 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+// contextAPI =>
+const AppContext = React.createContext()
+
+const AppProvider = ({ children }) => {
+  return (
+    <AppContext.Provider value='hello world'>{children}</AppContext.Provider>
+  )
+}
+
+// custom hook =>
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
