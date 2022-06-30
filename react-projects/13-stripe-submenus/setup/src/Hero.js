b89a95fde0e936b55c1a66ee59f09e8e39761d4e
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-06-30 11:15:54 +0200

    created base structure of navbar

--- react-projects/13-stripe-submenus/setup/src/Hero.js
+++ react-projects/13-stripe-submenus/setup/src/Hero.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
 
+// context import
+import { useGlobalContext } from './context'
+
 const Hero = () => {
+  const context = useGlobalContext()
+  console.log('context: ', context)
+
   return <h2>hero component</h2>
 }
 
--- react-projects/13-stripe-submenus/setup/src/Navbar.js
+++ react-projects/13-stripe-submenus/setup/src/Navbar.js
@@ -2,8 +2,38 @@ import React from 'react'
 import logo from './images/logo.svg'
 import { FaBars } from 'react-icons/fa'
 
+// context import
+import { useGlobalContext } from './context'
+
 const Navbar = () => {
-  return <h2>navbar component</h2>
+  const { openSidebar } = useGlobalContext()
+
+  return (
+    <nav className='nav'>
+      <div className='nav-center'>
+        {/* NAVBAR */}
+        <div className='nav-header'>
+          <img src={logo} alt='stripe' className='nav-logo' />
+          <button className='btn toggle-btn' onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+
+        {/* NAVBAR items */}
+        <ul className='nav-links'>
+          <li>
+            <button className='link-btn'>projects</button>
+          </li>
+          <li>
+            <button className='link-btn'>devolper journey</button>
+          </li>
+          <li>
+            <button className='link-btn'>company</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar
