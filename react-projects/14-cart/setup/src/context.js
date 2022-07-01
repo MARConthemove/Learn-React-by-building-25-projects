ccbc68aa53044ade9e557891c2f0d207cb144c5c
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-01 13:26:29 +0200

    Starting to build a small cart app, chose a product from a database, amount, price and so on

--- react-projects/14-cart/setup/src/context.js
+++ react-projects/14-cart/setup/src/context.js
@@ -3,7 +3,9 @@ import cartItems from './data'
 import reducer from './reducer'
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
+
 const url = 'https://course-api.com/react-useReducer-cart-project'
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
