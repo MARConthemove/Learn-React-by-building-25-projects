3dda154ce0bffbfe2755273cbc37da31456fefaf
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-02 11:34:55 +0200

    created custom hook for fetching data

--- react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ react-advanced-2020/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,17 +1,27 @@
 import { useState, useEffect } from 'react'
 
-export const useFetch = () => {
+export const useFetch = (url) => {
   const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  // data container
   const [products, setProducts] = useState([])
 
   const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
+    try {
+      const response = await fetch(url)
+      const products = await response.json()
+      setProducts(products)
+      setLoading(false)
+    } catch (error) {
+      setError(true)
+      console.log(error)
+    }
   }
 
   useEffect(() => {
     getProducts()
   }, [url])
+
+  return { loading, products, error }
 }
