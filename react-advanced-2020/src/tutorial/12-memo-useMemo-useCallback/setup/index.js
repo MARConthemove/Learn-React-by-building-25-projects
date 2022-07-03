db141bac6d2cfe8b64f259d91f06b9f3ec405b19
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-03 15:47:41 +0200

    adding a useMemo() hook to the project

--- react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ react-advanced-2020/src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -6,15 +6,29 @@ import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
+const calculateMostExpensive = (data) => {
+  console.log('hello world')
+  return (
+    data.reduce((total, item) => {
+      const price = item.fields.price
+      if (price >= total) {
+        total = price
+      }
+      return total
+    }, 0) / 100
+  )
+}
 
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
   const [cart, setCart] = useState(0)
 
-  const addToCart = () => {
+  // if the rerender comes from props (not state change) we can use the useCallback hook by
+  // simply wrappring our function with it, and giving a depencie array
+  const addToCart = useCallback(() => {
     setCart(cart + 1)
-  }
+  }, [cart])
 
   // console.log('products:', products)
   return (
@@ -23,14 +37,16 @@ const Index = () => {
       <button className='btn' onClick={() => setCount(count + 1)}>
         click me
       </button>
-      <BigList products={products} />
+      <h1 style={{ marginTop: '3rem' }}>cart: {cart}</h1>
+      <h1>Most Expensive: ${calculateMostExpensive(products)}</h1>
+      <BigList products={products} addToCart={addToCart} />
     </>
   )
 }
 
 // React.memo (memo Function) is checking if "products" changes, and if not it will not rerender if any other
 // part of the application triggers a rerender like useState changes.
-const BigList = React.memo(({ products }) => {
+const BigList = React.memo(({ products, addToCart }) => {
   useEffect(() => {
     console.log('big list called')
   })
@@ -39,13 +55,19 @@ const BigList = React.memo(({ products }) => {
   return (
     <section className='products'>
       {products.map((product) => {
-        return <SingleProduct key={product.id} {...product}></SingleProduct>
+        return (
+          <SingleProduct
+            key={product.id}
+            {...product}
+            addToCart={addToCart}
+          ></SingleProduct>
+        )
       })}
     </section>
   )
 })
 
-const SingleProduct = ({ fields }) => {
+const SingleProduct = ({ fields, addToCart }) => {
   useEffect(() => {
     console.count('single item called')
   })
@@ -60,6 +82,7 @@ const SingleProduct = ({ fields }) => {
       <img src={image} alt={name} />
       <h4>{name}</h4>
       <p>${price}</p>
+      <button onClick={addToCart}>add to cart</button>
     </article>
   )
 }
