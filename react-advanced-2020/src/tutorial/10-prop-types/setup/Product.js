ec9dcb72da3792d5c5889b14650aa83f12b17678
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-03 14:04:01 +0200

    implemented default props

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({ name, image, price }) => {
   console.log(name, image, price)
   return (
     <article className='product'>
-      {/* <h4>{name}</h4>
+      <h4>{name}</h4>
       <p>${price}</p>
-      <img src={image.url} alt={name} /> */}
+      <img src={image.url} alt={name} />
     </article>
   )
 }
@@ -16,6 +17,12 @@ Product.propTypes = {
   image: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+}
+
+Product.defaultProps = {
+  name: 'default name',
+  price: 3.99,
+  image: defaultImage,
 }
 
 export default Product
