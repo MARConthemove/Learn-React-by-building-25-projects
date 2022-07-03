78001d9f54e3c744af9ec1fecdb7be57b38cde70
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-03 13:27:41 +0200

    prop types created

--- react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
+++ react-advanced-2020/src/tutorial/10-prop-types/setup/Product.js
@@ -1,13 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const Product = ({ name, image, price }) => {
+  console.log(name, image, price)
   return (
     <article className='product'>
-      <h4>{name}</h4>
+      {/* <h4>{name}</h4>
       <p>${price}</p>
-      <img src={image.url} alt={name} />
+      <img src={image.url} alt={name} /> */}
     </article>
   )
+}
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
 }
 
 export default Product
