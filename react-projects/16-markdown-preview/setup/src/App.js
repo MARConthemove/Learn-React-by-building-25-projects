5fcfa15f8c4622cd425600f8d92bf70d3ee7aa44
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-06 09:53:12 +0200

    finished markdown project app

--- react-projects/16-markdown-preview/setup/src/App.js
+++ react-projects/16-markdown-preview/setup/src/App.js
@@ -1,12 +1,32 @@
 import React, { useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 
+let testString = `### markdown preview
+---
+
+**bold**
+*italicized text*
+
+> blockquote
+
+${`code style syntax`}
+
+[MARConthemove LINK](https://github.com/MARConthemove)`
+
 function App() {
+  const [markdown, setMarkdown] = useState(testString)
+
   return (
     <main>
       <section className='markdown'>
-        <textarea name='' id='' cols='30' rows='10'></textarea>
-        <article></article>
+        <textarea
+          className='input'
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        ></textarea>
+        <article className='result'>
+          <ReactMarkdown children={markdown} />
+        </article>
       </section>
     </main>
   )
