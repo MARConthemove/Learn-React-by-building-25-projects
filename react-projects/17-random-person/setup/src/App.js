6e29e9baa1082fcdc65d3b59344c4871a27f16d0
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-06 11:43:53 +0200

    RandPerson: created fetchData function, object destructured API data, created new object

--- react-projects/17-random-person/setup/src/App.js
+++ react-projects/17-random-person/setup/src/App.js
@@ -23,12 +23,114 @@ function App() {
     console.log(e.target)
   }
 
+  // fetch function
+  const getPerson = async () => {
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      const person = data.results[0]
+
+      // email, person
+      const { phone, email } = person
+      // syntax destructuring => take large property rename it in image!
+      const { large: image } = person.picture
+      // different way to destructure! how to get nesten property's out of the object
+      const {
+        login: { password },
+      } = person
+      // name => first, last
+      const { first, last } = person.name
+      // age
+      const {
+        dob: { age },
+      } = person
+      // street => streetName, streetNumber
+      const {
+        location: {
+          street: { name: streetName, number: streetNumber },
+        },
+      } = person
+
+      const newPerson = {
+        image,
+        phone,
+        email,
+        password,
+        age,
+        street: `${streetName} ${streetNumber}`,
+        name: `${first} ${last}`,
+      }
+      console.log(newPerson)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // effects
+  useEffect(() => {
+    getPerson()
+  }, [])
+
   return (
     <main>
       <div className='block bcg-black'></div>
       <div className='block'>
         <div className='container'>
-          <img src={defaultImage} alt='' />
+          <img
+            src={(person && person.image) || defaultImage}
+            alt='random user'
+            className='user-img'
+          />
+          <p className='user-title'>my {title} is</p>
+          <p className='user-value'>{value}</p>
+          <div className='values-list'>
+            <button
+              className='icon'
+              data-label='name'
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+
+            <button
+              className='icon'
+              data-label='email'
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+
+            <button className='icon' data-label='age' onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+
+            <button
+              className='icon'
+              data-label='street'
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+
+            <button
+              className='icon'
+              data-label='phone'
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+
+            <button
+              className='icon'
+              data-label='password'
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className='btn' type='button'>
+            {loading ? 'loading...' : 'random user'}
+          </button>
         </div>
       </div>
     </main>
@@ -36,52 +138,3 @@ function App() {
 }
 
 export default App
-/*
-return (
-  <main>
-    <div className='block bcg-black'></div>
-    <div className='block'>
-      <div className='container'>
-        <img
-          src={(person && person.image) || defaultImage}
-          alt='random user'
-          className='user-img'
-        />
-        <p className='user-title'>My {title} is</p>
-        <p className='user-value'>{value}</p>
-        <div className='values-list'>
-          <button className='icon' data-label='name' onMouseOver={handleValue}>
-            <FaUser />
-          </button>
-          <button className='icon' data-label='email' onMouseOver={handleValue}>
-            <FaEnvelopeOpen />
-          </button>
-          <button className='icon' data-label='age' onMouseOver={handleValue}>
-            <FaCalendarTimes />
-          </button>
-          <button
-            className='icon'
-            data-label='street'
-            onMouseOver={handleValue}
-          >
-            <FaMap />
-          </button>
-          <button className='icon' data-label='phone' onMouseOver={handleValue}>
-            <FaPhone />
-          </button>
-          <button
-            className='icon'
-            data-label='password'
-            onMouseOver={handleValue}
-          >
-            <FaLock />
-          </button>
-        </div>
-        <button className='btn' type='button' onClick={getPerson}>
-          {loading ? 'loading...' : 'random user'}
-        </button>
-      </div>
-    </div>
-  </main>
-)
-*/
