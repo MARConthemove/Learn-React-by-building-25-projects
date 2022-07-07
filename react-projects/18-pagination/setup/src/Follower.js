9e13adccc04ea438adf0ab7955d2eb7284b1f1ec
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-07 13:53:49 +0200

    finished pagination project: finally could fix bugs

--- react-projects/18-pagination/setup/src/Follower.js
+++ react-projects/18-pagination/setup/src/Follower.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-// or {html_url}! but also working with a template string
-const Follower = ({ avatar_url, login, url }) => {
+const Follower = ({ avatar_url, html_url, login }) => {
   return (
     <article className='card'>
       <img src={avatar_url} alt={login} />
       <h4>{login}</h4>
-      <a href={`https://github.com/${login}`} className='btn'>
+      <a href={html_url} className='btn'>
         view profile
       </a>
     </article>
