5bb5cf771b0118b162c52a8d2b5d085547b0e339
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-07 10:54:36 +0200

    Pagination GitHub followers: started project, created container and fetch data

--- react-projects/18-pagination/setup/src/Follower.js
+++ react-projects/18-pagination/setup/src/Follower.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Follower = () => {
-  return <h2>follower component</h2>
+// or {html_url}! but also working with a template string
+const Follower = ({ avatar_url, login, url }) => {
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login} />
+      <h4>{login}</h4>
+      <a href={`https://github.com/${login}`} className='btn'>
+        view profile
+      </a>
+    </article>
+  )
 }
 
 export default Follower
