3a29dfabf1b05fb5940c32df16b06c3224bb0700
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-10 14:33:58 +0200

    finished project picute loading

--- react-projects/19-stock-photos/setup/src/App.js
+++ react-projects/19-stock-photos/setup/src/App.js
@@ -19,13 +19,15 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 // fix scroll functionality
 
 function App() {
-  // state
+  // state:
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
   const [page, setPage] = useState(1)
   const [query, setQuery] = useState('')
+  const [newImages, setNewImages] = useState(false)
   const mounted = useRef(false)
 
+  // fetchData function:
   const fetchImages = async () => {
     setLoading(true)
     // create fetch url
@@ -54,11 +56,12 @@ function App() {
           return [...oldPhotos, ...data]
         }
       })
+      setNewImages(false)
       setLoading(false)
     } catch (error) {
+      setNewImages(false)
       setLoading(false)
       console.log(error)
-      console.log(error.response)
     }
   }
 
@@ -70,27 +73,28 @@ function App() {
 
   // This effect should only render with the 2, 3, ... page and not initially -> useRef()
   useEffect(() => {
-    console.log(mounted)
+    // not running on the initial render
     if (!mounted.current) {
       mounted.current = true
       return
     }
-    console.log('second')
-  }, [])
+    if (!newImages) return
+    if (loading) return
+    setPage((oldPage) => oldPage + 1)
+  }, [newImages])
 
-  // Old useEffect, buggy because loading was always false
-  /*   useEffect(() => {
-    const event = window.addEventListener('scroll', () => {
-      if (
-        !loading &&
-        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
-      ) {
-        setPage((oldPage) => oldPage + 1)
-      }
-    })
-    return () => window.removeEventListener('scroll', event)
-    // eslint-disable-next-line
-  }, []) */
+  const event = () => {
+    if (window.innerHeight + window.scrollY >= document.body.scrollHeight - 2) {
+      setNewImages(true)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', event)
+    return () => {
+      window.removeEventListener('scroll', event)
+    }
+  }, [])
 
   // handlers
   const handleSubmit = (e) => {
@@ -134,3 +138,19 @@ function App() {
 }
 
 export default App
+
+// old code :
+
+// Old useEffect, buggy because loading was always false
+/*   useEffect(() => {
+    const event = window.addEventListener('scroll', () => {
+      if (
+        !loading &&
+        window.innerHeight + window.scrollY >= document.body.scrollHeight - 2
+      ) {
+        setPage((oldPage) => oldPage + 1)
+      }
+    })
+    return () => window.removeEventListener('scroll', event)
+    // eslint-disable-next-line
+  }, []) */
