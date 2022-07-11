37c0e310107f693ecd1902f83eac34c3bc814761
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-11 10:00:22 +0200

    setting up api key for movie app

--- react-projects/21-movie-db/setup/src/context.js
+++ react-projects/21-movie-db/setup/src/context.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext, useEffect } from 'react'
-// make sure to use https
+
+// access to .env file variable via process.env.
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
+
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
-// make sure use
+
+// customHook to connect context:
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
