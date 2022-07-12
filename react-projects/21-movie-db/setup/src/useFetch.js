03912888d728cdba7fbb8b4e2838e8b05ad01c30
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-12 15:43:50 +0200

    refactoring context and useFetch customHook

--- react-projects/21-movie-db/setup/src/useFetch.js
+++ react-projects/21-movie-db/setup/src/useFetch.js
@@ -2,6 +2,37 @@ import React, { useState, useEffect } from 'react'
 
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
 
-export const useFetch = () => {
-  return <div>useFetch</div>
+const useFetch = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [error, setError] = useState({ show: false, msg: '' })
+  const [movies, setMovies] = useState([])
+
+  const fetchMovies = async (urlParams) => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log('data: ', data)
+
+      // errorHandling:
+      if (data.Response === 'True') {
+        setMovies(data.Search)
+        setError({ show: false, msg: '' })
+      } else {
+        setError({ show: true, msg: data.Error })
+      }
+
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}${urlParams}`)
+  }, [urlParams])
+
+  return {}
 }
+
+export default useFetch
