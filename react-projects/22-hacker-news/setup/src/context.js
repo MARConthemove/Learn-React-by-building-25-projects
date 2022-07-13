a770730010e7b3d364bee1d669cad5da1a1368b5
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-13 11:56:36 +0200

    Hacker news: setting up the context and useReducer hook for functionality of the app

--- react-projects/22-hacker-news/setup/src/context.js
+++ react-projects/22-hacker-news/setup/src/context.js
@@ -7,18 +7,32 @@ import {
   HANDLE_PAGE,
   HANDLE_SEARCH,
 } from './actions'
+
+// reducer function
 import reducer from './reducer'
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?'
+// &query=...
+// &page=...
 
+// reducer initialState
 const initialState = {}
 
+// context object
 const AppContext = React.createContext()
 
+// context provider
 const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState, third)
+
+  const fetchStories = async () => {
+    dispatch()
+  }
+
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
-// make sure use
+
+// custom hook
 export const useGlobalContext = () => {
   return useContext(AppContext)
 }
