845b85ff27a11b091ddac974d4019a551ef9e546
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-15 10:28:14 +0200

    Initiated tempUrl for api call

--- react-projects/23-quiz/setup/src/context.js
+++ react-projects/23-quiz/setup/src/context.js
@@ -6,17 +6,27 @@ const table = {
   history: 23,
   politics: 24,
 }
+// showcase url:
+/* https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple */
 
 // New api endpoint, maybe needed to be changed later
 const API_ENDPOINT = 'https://opentdb.com/api.php?'
 
-const url = ''
+const url =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
+
+const tempUrl =
+  'https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple'
 
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+
   return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
 }
+
 // make sure use
 export const useGlobalContext = () => {
   return useContext(AppContext)
