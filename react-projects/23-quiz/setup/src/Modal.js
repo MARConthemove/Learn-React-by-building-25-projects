bf780005749e83fdbf89f614e991c1ce5d0b3eca
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-15 14:21:53 +0200

    Modal is opening at the end of a round and showing correctly % of correct answers

--- react-projects/23-quiz/setup/src/Modal.js
+++ react-projects/23-quiz/setup/src/Modal.js
@@ -13,6 +13,13 @@ const Modal = () => {
     >
       <div className='modal-content'>
         <h2>congrats!</h2>
+        <p>
+          You answered {((correct / questions.length) * 100).toFixed(0)}% of
+          questions correctly
+        </p>
+        <button className='close-btn' onClick={closeModal}>
+          play again
+        </button>
       </div>
     </div>
   )
