fc3d044c0c0d7aba13422bade17007bdb9193311
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-17 14:55:49 +0200

    Random functionality implemented, finished projects, ready for build

--- react-projects/23-quiz/setup/src/App.js
+++ react-projects/23-quiz/setup/src/App.js
@@ -27,9 +27,16 @@ function App() {
   const { question, correct_answer, incorrect_answers } = questions[index]
 
   // const answers = [...incorrect_answers, correct_answer]
+
   const answers = [...incorrect_answers]
   const tempIndex = Math.floor(Math.random() * 4)
-  console.log(tempIndex)
+
+  if (tempIndex === 3) {
+    answers.push(correct_answer)
+  } else {
+    answers.push(answers[tempIndex])
+    answers[tempIndex] = correct_answer
+  }
 
   return (
     <main>
