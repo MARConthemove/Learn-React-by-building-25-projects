e04afdc010abc4e059db4e0a0e7142cd6d612e4c
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-18 17:23:14 +0200

    Created login page

--- react-projects/24-github-search-users/src/pages/Dashboard.js
+++ react-projects/24-github-search-users/src/pages/Dashboard.js
@@ -1,13 +1,20 @@
-import React from 'react';
-import { Info, Repos, User, Search, Navbar } from '../components';
-import loadingImage from '../images/preloader.gif';
-import { GithubContext } from '../context/context';
+import React from 'react'
+import { Info, Repos, User, Search, Navbar } from '../components'
+import loadingImage from '../images/preloader.gif'
+import { GithubContext } from '../context/context'
+
+// components either with selfclosing tag or without
+
 const Dashboard = () => {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar></Navbar>
+      <Search />
+      <Info />
+      <User />
+      <Repos />
     </main>
-  );
-};
+  )
+}
 
-export default Dashboard;
+export default Dashboard
