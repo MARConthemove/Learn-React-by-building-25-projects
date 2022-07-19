3ddc87c7597e84b032f8231b74b53ba740636dc9
Marc - Fabian Gätcke <62877154+MARConthemove@users.noreply.github.com>
2022-07-19 17:12:29 +0200

    Started to intialize the Provider via Context

--- react-projects/24-github-search-users/src/context/context.js
+++ react-projects/24-github-search-users/src/context/context.js
@@ -1,7 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import mockUser from './mockData.js/mockUser'
+import mockRepos from './mockData.js/mockRepos'
+import mockFollowers from './mockData.js/mockFollowers'
+import axios from 'axios'
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com'
+
+// creating context for app
+const GithubContext = React.createContext()
+
+// Provider, Consumer - we can access them with GithubContext.Provider
+const GithubProvider = ({ children }) => {
+  return (
+    <GithubContext.Provider value={'hello'}>{children}</GithubContext.Provider>
+  )
+}
+
+export { GithubContext, GithubProvider }
